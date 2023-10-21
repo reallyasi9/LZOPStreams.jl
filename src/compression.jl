201fc9cd4504cd947f13c818e6b9079a72719d6d
Phil Killewald <reallyasi9@users.noreply.github.com>
2023-10-21 01:02:26 -0400

    Start adding compress algorithm
    Working off of https://github.com/airlift/aircompressor/blob/master/src/main/java/io/airlift/compress/lzo/LzoRawCompressor.java

--- src/compression.jl
+++ src/compression.jl
@@ -1,43 +1,119 @@
 abstract type LZOCompressorCodec <: TranscodingStreams.Codec end
 
-dictionary_type(::LZOCompressorCodec) = Nothing
-dictionary_bits(::LZOCompressorCodec) = nothing
-dictionary_index1(::LZOCompressorCodec, ::Integer, ::AbstractVector{UInt8}) = nothing
-dictionary_index2(::LZOCompressorCodec, ::Integer, ::AbstractVector{UInt8}) = nothing
-dictionary_index(::LZOCompressorCodec, ::Integer) = nothing
-
-dictionary_mask(c::LZOCompressorCodec) = (1 << dictionary_bits(c)) - 1
-dictionary_high(c::LZOCompressorCodec) = (1 << (dictionary_bits(c) - 1)) + 1
-
-
-struct LZO1X1CompressorCodec <: LZOCompressorCodec
+mutable struct LZO1X1CompressorCodec <: LZOCompressorCodec
     working::Vector{UInt8}
-    bytes_processed::Int
+    buffer::Vector{UInt8}
+    buffer_used::Int
     
-    LZO1X1CompressorCodec() = new(Vector{UInt8}(undef, 16384), 0)
+    LZO1X1CompressorCodec() = new(Vector{UInt8}(undef, MAX_TABLE_SIZE), Vector{UInt8}(undef, MAX_DISTANCE), 0)
 end
-
-dictionary_type(::LZO1X1CompressorCodec) = UInt16
-dictionary_bits(::LZO1X1CompressorCodec) = 14
-dictionary_index1(c::LZO1X1CompressorCodec, ::Integer, p::AbstractVector{UInt8}) = DM(dictionary_mask(c), (0x21 * DX3(p, 5, 5, 6)) >> 5)
-dictionary_index2(c::LZO1X1CompressorCodec, d::Integer, ::AbstractVector{UInt8}) = d & (dictionary_mask(c) & 0x7ff) ⊻ (dictionary_high(c) | 0x1f)
-dictionary_index(c::LZO1X1CompressorCodec, dv::Integer) = (0x1824429d * dv) >> (32 - dictionary_bits(c))
 
 function TranscodingStreams.initialize(codec::LZO1X1CompressorCodec)
     fill!(codec.working, 0)
-    codec.bytes_processed = 0
+    fill!(codec.buffer, 0)
+    codec.buffer_used = 0
     return
 end
 
-function TranscodingStreams.process(codec::LZO1X1CompressorCodec, input::Memory, output::Memory, error::Error)
-    n_read = 0
-    n_written = 0
-    status = :ok
+function buffer_input!(codec::LZO1X1CompressorCodec, input::Memory, start_idx::Int, error::Error)
+    @boundscheck if start_idx < 1 || start_idx > length(input)
+        error[] = ErrorException("input start index $start_idx out of bounds")
+        return 0, :error
+    end
+    @boundscheck if codec.buffer_used < 0 || codec.buffer_used > length(codec.buffer)
+        error[] = ErrorException("buffer used $(codec.buffer_used) out of bounds")
+        return 0, :error
+    end
+    input_remaining = length(input) - start_idx + 1
+    buffer_remaining = length(codec.buffer) - codec.buffer_used
+    to_copy = min(input_remaining, buffer_remaining)
+    @inbounds copyto!(codec.buffer, codec.buffer_used+1, input, start_idx, to_copy)
+    return to_copy, :ok
+end
 
-    # Processing is done in chunks of 49152 bytes
-    if length(input) == 0
-        # acts as an instruction to end processing
+function compute_table_size(l::Int)
+    # smallest power of 2 larger than l
+    target = one(l) << ((sizeof(l)*8 - leading_zeros(l-one(l))) + 1)
+    return clamp(target, MIN_TABLE_SIZE, MAX_TABLE_SIZE)
+end
+
+function compress_chunk!(codec::LZO1X1CompressorCodec, input::Memory, input_start::Int, output::Memory, output_start::Int, error::Error)
+    input_length = length(input) - input_start + 1
+
+    # nothing compresses to nothing
+    # This should never happen
+    if input_length == 0
+        return 0, 0, :ok
     end
 
-    return n_read, n_written, status
+    # inputs that are smaller than the shortest lookback distance are emitted as literals
+    if input_length < MIN_LENGTH
+        return emit_last_literal(input, input_start, output, output_start, error; first_literal = true)
+    end
+
+    # build a working table
+    table_size = compute_table_size(input_length)
+    mask = table_size - 1
+    fill!(codec.working, 0)
+
+    # the very first byte is set in the table to the first memory offset (zero)
+    codec.working[hash(get_long(input, input_start), mask)] = 0
+
+    input_idx = input_start+1
+    next_hash = hash(get_long(input, input_idx), mask)
+end
+
+function TranscodingStreams.process(codec::LZO1X1CompressorCodec, input::Memory, output::Memory, error::Error)
+
+    n_read = 0
+    n_written = 0
+
+    # input length of zero means reading has hit EOF, so write out all buffers
+    input_length = length(input)
+    if input_length == 0
+        r, n_written, status = compress_chunk!(codec, codec.buffer, 1, output, n_written+1, error)
+        codec.buffer_used -= r
+        if status == :ok
+            status = :end
+        end
+        return 0, n_written, status
+    end
+
+    # if the buffer has data in it, try to fill it with input
+    if codec.buffer_used > 0
+        r, status = buffer_input!(codec, input, n_read+1, error)
+        n_read += r
+        if status != :ok
+            return n_read, n_written, status
+        end
+    end
+
+    # if the buffer is full, dump it
+    if codec.buffer_used == length(codec.buffer_used)
+        r, w, status = compress_chunk!(codec, codec.buffer, 1, output, n_written+1, error)
+        codec.buffer_used -= r
+        n_written += w
+        if status != :ok
+            return n_read, n_written, status
+        end
+    end
+
+    # with everything else, process one chunk at a time
+    while length(input) - n_read >= MAX_DISTANCE
+        r, w, status = compress_chunk!(codec, input, n_read+1, output, n_written+1, error)
+        n_read += r
+        n_written += w
+        if status != :ok
+            return n_read, n_written, status
+        end
+    end
+
+    # if anything else is left, buffer it until the next call to process
+    if length(input) - n_read > 0
+        r, status = buffer_input!(codec, input, n_read+1, error)
+        n_read += r
+    end
+    
+    # done: wait for next call to process
+    return n_read, n_written, :ok
 end
--- src/dict.jl
+++ src/dict.jl
@@ -1,7 +1,36 @@
-# Dictionary functions from lzo_dict.h
+# Dictionary definitions
 
-LZO_SIZE(bits) = 1 << bits
-LZO_MASK(bits) = LZO_SIZE(bits) - 1
-DX2(p::AbstractVector{UInt8}, s1::Integer, s2::Integer) = @inbounds (((p[3] << s2) ⊻ p[2]) << s1) ⊻ p[1]
-DX3(p::AbstractVector{UInt8}, s1::Integer, s2::Integer, s3::Integer) = @inbounds (((((p[4] << s3) ⊻ p[3]) << s2) ⊻ p[2]) << s1) ⊻ p[1]
-DM(mask, v, s::Integer=0) = (v & (mask >> s)) << s
+const SHORT_SIZE = 2
+const INT_SIZE = 4
+const LONG_SIZE = 8
+
+const LAST_LITERAL_SIZE = 5  # the number of bytes in the last literal
+const MIN_MATCH = 4  # the smallest number of bytes to consider in a dictionary lookup
+const MAX_INPUT_SIZE = 0x7e000000  # 2133929216 bytes
+const HASH_LOG = 12  # 1 << 12 = 4 KB maximum dictionary size
+const MIN_TABLE_SIZE = 16
+const MAX_TABLE_SIZE = 1 << HASH_LOG
+const COPY_LENGTH = 8  # copy this many bytes at a time, if possible
+const MATCH_FIND_LIMIT = COPY_LENGTH + MIN_MATCH  # 12
+const MIN_LENGTH = MATCH_FIND_LIMIT + 1
+const ML_BITS = 4  # match lengths can be up to 1 << 4 - 1 = 15
+const RUN_BITS = 8 - ML_BITS  # runs can be up to 1 << 4 - 1 = 15
+const RUN_MASK = (1 << RUN_BITS) - 1
+
+const MAX_DISTANCE = 0b1100000000000000 - 1  # 49151 bytes, this is the maximum chunk size allowed before the dictionary has to be rebuilt
+const SKIP_TRIGGER = 6  # This tunes the compression ratio: higher values increases the compression but runs slower on incompressable data
+
+# Perform `value * frac(a)` for `a` with a good mix of 1s and 0s in its binary representation.
+function hash(value::Int64, mask::Int32)
+    return ((value * 889523592379 >>> 28) & mask) % Int32
+end
+
+# Compute the minimum safe compressed length required
+function max_compressed_length(in_length::Integer)
+    return in_length + in_length/255 + 16
+end
+
+# Get 8 bytes from a memory object and reinterpret as a LE Int64
+function get_long(input::Memory, index::Int)
+    return ltoh(first(reinterpret(Int64, unsafe_wrap(Vector{UInt8}, input.ptr+index-1, 8))))
+end
