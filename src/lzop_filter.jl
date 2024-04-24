e5fcaee83c022fe5638b70ed7830049fe98f5a6b
--get <reallyasi9@users.noreply.github.com>
2024-04-24 19:45:10 -0400

    Convert filters to types

--- src/lzop_filter.jl
+++ src/lzop_filter.jl
@@ -1,150 +1,126 @@
-@enum FilterType begin
-    NO_FILTER = 0
-    MOD1_FILTER = 1
-    MOD2_FILTER
-    MOD3_FILTER
-    MOD4_FILTER
-    MOD5_FILTER
-    MOD6_FILTER
-    MOD7_FILTER
-    MOD8_FILTER
-    MOD9_FILTER
-    MOD10_FILTER
-    MOD11_FILTER
-    MOD12_FILTER
-    MOD13_FILTER
-    MOD14_FILTER
-    MOD15_FILTER
-    MOD16_FILTER
-    MTF_FILTER = typemax(Int32)
+abstract type AbstractLZOPFilter end
+
+"""
+    ModuloSumFilter{N}
+
+    Apply a reversable modulo sum mapping.
+
+Performs the mapping `data[i] <- (data[i] + data[i-N]) % 256` for all indices `i ∈ 1:length(data)`. All out-of-bounds values are treated as zeros.
+
+## Template parameters
+- `N::Integer`: The number of sums to cycle.
+"""
+struct ModuloSumFilter{N}::AbstractLZOPFilter
+    cache::Vector{UInt8}
+
+    function ModuloSumFilter{N}() where {N}
+        return new{N}(Vector{UInt8}(undef, N))
+    end
 end
 
 """
-    mod_filter!(data, n::Integer)
+    MoveToFrontFilter
 
-    Apply a reversable modulo sum mapping to iterable `data` in-place and return `data`.
+    Apply a reversable move-to-front mapping.
+"""
+struct MoveToFrontFilter::AbstractLZOPFilter
+    dict::Vector{UInt8}
 
-Performs the mapping `data[i] <- (data[i] + data[i-n]) % 256` for all indices `i ∈ 1:length(data)`. All out-of-bounds values are treated as zeros.
+    function MoveToFrontFilter()
+        return new(Vector{UInt8}(undef, 256))
+    end
+end
+
+"""
+    lzop_filter!(f, data) -> data
+
+    Apply a reversable mapping to `data` in-place and return `data`.
 
 ## Arguments
-- `data`: An array-like object that implements `getindex`, `setindex`, and `length`, and stores `UInt8` values.
-- `n::Integer`: The number of sums to cycle.
+- `f::AbstractLZOPFilter`: A struct specific to the kind of LZOP filter to apply to the data.
+- `data::AbstractArray{UInt8}`: An array-like object of data to which the mapping is applied.
 """
-function mod_filter!(data::AbstractArray{UInt8}, n::Integer)
-    n == 1 && return mod1_filter!(data)
-    cache = zeros(UInt8, n)
-    for i in eachindex(data)
-        j = mod1(i, n)
-        cache[j] += data[i]
-        data[i] = cache[j]
+function lzop_filter!(f::ModuloSumFilter{N}, data::AbstractVector{UInt8}) where {N}
+    fill!(f.cache, zero(UInt8))
+    i = 0
+    for j in eachindex(data)
+        i = mod1(i+1, N)
+        f.cache[i] += data[j]
+        data[j] = f.cache[i]
     end
     return data
 end
 
-function mod1_filter!(data::AbstractArray{UInt8})
-    cache = UInt8(0)
-    for i in eachindex(data)
-        cache += data[i]
-        data[i] = cache
+# special, faster version for N=1 case
+function lzop_filter!(::ModuloSumFilter{1}, data::AbstractVector{UInt8})
+    cache = zero(UInt8)
+    for j in eachindex(data)
+        cache += data[j]
+        data[j] = cache
     end
     return data
 end
 
-"""
-    unmod_filter!(data, n::Integer)
+function lzop_filter!(f::MoveToFrontFilter, data::AbstractArray{UInt8})
+    f.dict .= UInt8.(0:255)
 
-    Reverse the reversable modulo sum mapping to iterable `data` in-place and return `data`.
-
-Performs the mapping `data[i] <- (data[i] - data[i-n]) % 256` for all indices `i ∈ 1:length(data)`. All out-of-bounds values are treated as zeros.
-
-## Arguments
-- `data`: An array-like object that implements `getindex`, `setindex`, and `length`, and stores `UInt8` values.
-- `n::Integer`: The number of sums to cycle.
-"""
-function unmod_filter!(data::AbstractArray{UInt8}, n::Integer)
-    n == 1 && return unmod1_filter!(data)
-    cache = zeros(UInt8, n)
-    for i in eachindex(data)
-        j = mod1(i, n)
-        data[i] -= cache[j]
-        cache[j] += data[i]
-    end
-    return data
-end
-
-function unmod1_filter!(data::AbstractArray{UInt8})
-    cache = UInt8(0)
-    for i in eachindex(data)
-        data[i] -= cache
-        cache += data[i]
-    end
-    return data
-end
-
-"""
-    mtf_filter!(data)
-
-    Apply a reversable move-to-front mapping to iterable `data` in-place and return `data`.
-
-## Arguments
-- `data`: An array-like object that implements `getindex`, `setindex`, and `eachindex`, and stores `UInt8` values.
-"""
-function mtf_filter!(data::AbstractArray{UInt8})
-    dict = UInt8.(0:255)
-
-    for i in eachindex(data)
-        cache = data[i]
-        j = (findfirst(==(cache), dict) - 1) % UInt8
-        data[i] = j
-        while j > 0
-            dict[j+1] = dict[j]
-            j -= 1
+    for j in eachindex(data)
+        cache = data[j]
+        i = (findfirst(==(cache), f.dict) - 1) % UInt8
+        data[j] = i
+        while i > 0
+            f.dict[i+1] = f.dict[i]
+            i -= 1
         end
-        dict[1] = cache
+        f.dict[1] = cache
     end
 
     return data
 end
 
 """
-    unmtf_filter!(data)
+    lzop_unfilter!(f, data) -> data
 
-    Reverse a reversable move-to-front mapping of iterable `data` in-place and return `data`.
+    Reverse the mapping to `data` in-place and return `data`.
 
 ## Arguments
-- `data`: An array-like object that implements `getindex`, `setindex`, and `eachindex`, and stores `UInt8` values.
+- `f::AbstractLZOPFilter`: A struct specific to the kind of LZOP filter applied to the data.
+- `data::AbstractArray{UInt8}`: An array-like object of data to which the mapping was applied.
 """
-function unmtf_filter!(data::AbstractArray{UInt8})
-    dict = UInt8.(0:255)
-
-    for i in eachindex(data)
-        j = data[i] + 1
-        cache = dict[j]
-        data[i] = cache
-        while j > 1
-            dict[j] = dict[j-1]
-            j -= 1
-        end
-        dict[1] = cache
+function lzop_unfilter!(f::ModuloSumFilter{N}, data::AbstractArray{UInt8}) where {N}
+    fill!(f.cache, zero(UInt8))
+    i = 0
+    for j in eachindex(data)
+        i = mod1(i+1, N)
+        data[j] -= cache[i]
+        cache[i] += data[j]
     end
-
     return data
 end
 
-function lzop_filter!(data::AbstractArray{UInt8}, filter::FilterType)
-    length(data) == 0 && return data
-    filter == NO_FILTER && return data
-    filter == MOD1_FILTER && return mod1_filter!(data)
-    filter < MTF_FILTER && return mod_filter!(data, filter)
-    filter == MTF_FILTER && return mtf_filter!(data)
-    throw(ArgumentError("unknown filter type $filter"))
+# special, faster version for N=1 case
+function lzop_unfilter!(::ModuloSumFilter{1}, data::AbstractArray{UInt8}) where {N}
+    cache = zero(UInt8)
+    for j in eachindex(data)
+        data[j] -= cache
+        cache += data[j]
+    end
+    return data
 end
 
-function lzop_unfilter!(data::AbstractArray{UInt8}, filter::FilterType)
-    length(data) == 0 && return data
-    filter == NO_FILTER && return data
-    filter == MOD1_FILTER && return unmod1_filter!(data)
-    filter < MTF_FILTER && return unmod_filter!(data, filter)
-    filter == MTF_FILTER && return unmtf_filter!(data)
-    throw(ArgumentError("unknown filter type $filter"))
+function lzop_unfilter!(f::MoveToFrontFilter, data::AbstractArray{UInt8})
+    f.dict .= UInt8.(0:255)
+
+    for j in eachindex(data)
+        i = data[j] + 1
+        cache = f.dict[i]
+        data[j] = cache
+        while i > 1
+            f.dict[i] = f.dict[i-1]
+            i -= 1
+        end
+        f.dict[1] = cache
+    end
+
+    return data
 end
